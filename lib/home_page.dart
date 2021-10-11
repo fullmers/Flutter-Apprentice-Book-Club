6c9a0c0d7f85ae1193cd703c118a75dc6cadd7a7
Sarah Fullmer <sarah.fullmer@gmail.com>
2021-10-11 13:21:02 +0200

    showed the apod image, date and title in a card. date is formatted

---
+++ lib/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:apod/apod.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Apod.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return (_buildApodCard(Apod.samples[index]));
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildApodCard(Apod apod) {
+    final String imageToShow =
+        apod.mediaType == MediaType.image ? apod.url! : apod.thumb!;
+    return Card(
+        elevation: 4,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: AssetImage(imageToShow),
+              ),
+            ),
+            Text(DateFormat('yyyy-MM-dd').format(apod.date!),
+                style: const TextStyle(
+                  fontSize: 14,
+                )),
+            Text(apod.title!,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(height: 8),
+          ],
+        ));
+  }
+}
