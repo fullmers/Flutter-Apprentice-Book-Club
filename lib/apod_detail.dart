2d7ca59981014fc635f7443287c604ec951d3665
Craig Labenz <craig.labenz@gmail.com>
2021-10-12 22:25:19 -0700

    update InteractiveViewer for better desktop scrolling

--- lib/apod_detail.dart
+++ lib/apod_detail.dart
@@ -25,85 +25,112 @@ class _ApodDetailState extends State<ApodDetail> {
         title: (Text(widget.apod.title!)),
       ),
       body: SafeArea(
-          child: ListView(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: InteractiveViewer(
-              transformationController: _controller,
-              child: Image(
-                image: AssetImage(widget.apod.url!),
+        child: Stack(
+          children: [
+            Positioned(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+              child: InteractiveViewer(
+                transformationController: _controller,
+                child: Image(
+                  image: AssetImage(widget.apod.url!),
+                  fit: BoxFit.fitWidth,
+                ),
               ),
             ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.4,
+              // 50 pixels is the size of the upper navbar. Subtracting
+              // this prevents the last part of our test from being
+              // unreachable below the bottom of the UI.
+              height: MediaQuery.of(context).size.height * 0.6 - 50,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                children: [
+                  Slider(
+                    min: 1.0,
+                    max: 4.0,
+                    divisions: 20,
+                    label: _sliderScalar.toStringAsPrecision(2),
+                    value: _sliderScalar.toDouble(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _sliderScalar = newValue;
+                        _controller.value =
+                            Matrix4.identity().scaled(_sliderScalar);
+                      });
+                    },
+                    activeColor: Colors.purple[200],
+                    inactiveColor: Colors.purple[800],
+                  ),
+                  _ApodBody(widget.apod, scalar: _sliderScalar),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ApodBody extends StatelessWidget {
+  const _ApodBody(this.apod, {required this.scalar});
+  final Apod apod;
+  final double scalar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                DateFormat('yyyy-MM-dd').format(apod.date!),
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Chip(
+                backgroundColor: Colors.purple,
+                label: Text(
+                  'Zoom: ${scalar.toStringAsPrecision(2)}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          Slider(
-            min: 1.0,
-            max: 4.0,
-            divisions: 20,
-            label: _sliderScalar.toStringAsPrecision(2),
-            value: _sliderScalar.toDouble(),
-            onChanged: (newValue) {
-              setState(() {
-                _sliderScalar = newValue;
-                _controller.value = Matrix4.identity().scaled(_sliderScalar);
-              });
-            },
-            activeColor: Colors.purple[200],
-            inactiveColor: Colors.purple[800],
+          const SizedBox(height: 4),
+          Text(
+            apod.title!,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      DateFormat('yyyy-MM-dd').format(widget.apod.date!),
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Chip(
-                      backgroundColor: Colors.purple,
-                      label: Text(
-                        'Zoom: ${_sliderScalar.toStringAsPrecision(2)}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  widget.apod.title!,
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  'Copyright: ${widget.apod.copyright!}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  widget.apod.explanation!,
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+          const SizedBox(height: 4),
+          Text(
+            'Copyright: ${apod.copyright!}',
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            apod.explanation!,
+            style: const TextStyle(
+              fontSize: 16,
             ),
           ),
         ],
-      )),
+      ),
     );
   }
 }
