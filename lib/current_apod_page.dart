772d09d46420e92d42c18a6d22514d8ddade04d7
Sarah Fullmer <sarah.fullmer@gmail.com>
2021-10-20 09:16:36 +0200

    add gesture detector to current_apod_page

--- lib/current_apod_page.dart
+++ lib/current_apod_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+
 import 'apod.dart';
 import 'apod_card.dart';
+import 'apod_detail.dart';
 
 class CurrentApodPage extends StatelessWidget {
   const CurrentApodPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ApodCard(Apod.samples[0]),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return (ApodDetail(apod: Apod.samples[0]));
+        }));
+      },
+      child: Center(
+        child: ApodCard(Apod.samples[0]),
+      ),
     );
   }
 }
