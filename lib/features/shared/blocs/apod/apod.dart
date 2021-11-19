97f3eeef80b5c2433120d547bbbbf8f7c6204269
Craig Labenz <craig.labenz@gmail.com>
2021-11-19 10:49:41 -0800

    added apod bloc

---
+++ lib/features/shared/blocs/apod/apod.dart
@@ -0,0 +1,3 @@
+export 'apod_bloc.dart';
+export 'apod_state.dart';
+export 'apod_event.dart';
---
+++ lib/features/shared/blocs/apod/apod_bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:apod/features/shared/models/models.dart';
+
+import 'apod_state.dart';
+import 'apod_event.dart';
+
+class ApodBloc {
+  ApodBloc(this._repository)
+      : _stateController = StreamController<ApodState>.broadcast(),
+        _eventsController = StreamController<ApodEvent>() {
+    _eventsController.stream.listen(_processEvent);
+  }
+
+  final Repository<Apod> _repository;
+  final StreamController<ApodState> _stateController;
+  final StreamController<ApodEvent> _eventsController;
+  ApodState? state;
+
+  void _processEvent(ApodEvent event) {}
+
+  Stream<ApodState> get stream => _stateController.stream;
+  void add(ApodEvent event) => _eventsController.sink.add(event);
+}
